8565e404fdc787097674da4a8230813e40e141b3
DragonsTeamInd <31885586+DragonsTeamInd@users.noreply.github.com>
2017-09-13 14:26:24 +0300

    Update ArmorDataSaver.py

--- ArmorDataSaver.py
+++ ArmorDataSaver.py
@@ -2,12 +2,12 @@ dicto = dict()
 armors = int
 dicto['LeatherArmor'] = (6,'Images/Armor/LeatherArmor.png','Images/Inventory/ArmorIco.png','Images/Armor/EquipedLeatherArmor.png','Images/Armor/LeatherArmor1.png','LeatherArmor')
 def openarmor(Armors,Armor):
-    with open('Armors.txt','r') as f:
+    with open('ArmorsData.txt','r') as f:
         armors = int(f.readline().strip( '\n' ))
         for i in range(armors):
             Armor = Armor(dicto[f.readline().strip( '\n' )],Armors)
 def savearmor(Armors):
-    with open('Armors.txt','w') as f:
+    with open('ArmorsData.txt','w') as f:
         f.write(str(len(Armors) + '\n'))
         for arm in Armors:
             f.write(arm.name + '\n')
