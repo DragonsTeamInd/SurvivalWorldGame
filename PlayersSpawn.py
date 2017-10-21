590472556fceba9dc390eaef47c5a11263758620
DragonsTeamInd <develierol@mail.ru>
2017-10-21 15:59:53 +0300

    New ground system

--- PlayersSpawn.py
+++ PlayersSpawn.py
@@ -4,5 +4,5 @@ from pygame import *
 class PlayersSpawn():
     def SpawnOnGround(self,ground,player):
         for gr in ground:
-            if abs(gr.rect.left - inf.screenwidth // 2) <= 72:
-                player.rect.top = gr.rect.top - (72 + gr.rect.height)
+            if abs(gr[0] - inf.screenwidth // 2) <= 72:
+                player.rect.top = gr[1] - (72 + inf.Ground_Block_Height)
